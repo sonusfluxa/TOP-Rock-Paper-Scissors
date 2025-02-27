d85b3ea66bc3cda90df37638245c2ae91b5ae1c7
sonusfluxa <31449573+sonusfluxa@users.noreply.github.com>
2025-02-27 11:58:54 -0500

    add js for computer choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,20 @@
 console.log("hello world");
+
+let randomNumber = Math.random();
+
+function getComputerChoice(randomNumber) {
+    if (randomNumber < .33) {
+        return "rock"
+    }
+    else if (randomNumber >=.33 && randomNumber < .66) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
+
+var computerChoice = getComputerChoice(randomNumber);
+
+console.log(randomNumber);
+console.log(computerChoice);
