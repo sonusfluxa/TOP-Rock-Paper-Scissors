2b722df3d46e1ca8b30574727513565a2dccdccd
sonusfluxa <31449573+sonusfluxa@users.noreply.github.com>
2025-02-27 15:43:04 -0500

    clean up round logic and add five round game logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,6 @@
-console.log("hello world");
+function getComputerChoice() {
+    let randomNumber = Math.random();
 
-let randomNumber = Math.random();
-
-function getComputerChoice(randomNumber) {
     if (randomNumber < .33) {
         return "rock"
     }
@@ -13,10 +11,6 @@ function getComputerChoice(randomNumber) {
         return "scissors"
     }
 }
-
-let computerChoice = getComputerChoice(randomNumber);
-
-// prompt("rock paper scissors?")
 
 function getHumanChoice() {
     let choice = prompt("rock paper scissors?").toLowerCase()
@@ -33,52 +27,86 @@ function getHumanChoice() {
     else return "try again"
 }
 
-let humanChoice = getHumanChoice();
-
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(computerChoice, humanChoice) {
-if (computerChoice == "rock" && humanChoice == "paper") {
-    console.log("paper beats rock, you win!");
-    ++humanScore ;
-}
-else if (computerChoice == "rock" && humanChoice == "scissors") {
-    console.log("rock beats scissors, you lose!");
-    ++computerScore ;
-}
-else if (computerChoice == "paper" && humanChoice == "scissors") {
-    console.log("scissors beats paper, you win!");
-    ++humanScore ;
-}
-else if (computerChoice == "paper" && humanChoice == "rock") {
-    console.log("paper beats rock, you lose!");
-    ++computerScore ;
-}
-else if (computerChoice == "scissors" && humanChoice == "rock") {
-    console.log("rock beats scissors, you win!");
-    ++humanScore ;
-}
-else if (computerChoice == "scissors" && humanChoice == "paper") {
-    console.log("scissors beats paper, you lose!");
-    ++computerScore ;
-}
-else if (computerChoice == humanChoice) {
-    console.log("tie!")
-}
-else {
-    console.log("try again!")
-}
+let computerChoice;
+let humanChoice;
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(computerChoice, humanChoice) {
+
+        computerChoice = getComputerChoice();
+        humanChoice = getHumanChoice();
+
+        console.log("COMPUTER CHOOSES: " + computerChoice);
+        console.log("YOU CHOOSE: " + humanChoice);
+
+        if (computerChoice == "rock" && humanChoice == "paper") {
+            console.log("paper beats rock, you win!");
+            ++humanScore ;
+        }
+        else if (computerChoice == "rock" && humanChoice == "scissors") {
+            console.log("rock beats scissors, you lose!");
+            ++computerScore ;
+        }
+        else if (computerChoice == "paper" && humanChoice == "scissors") {
+            console.log("scissors beats paper, you win!");
+            ++humanScore ;
+        }
+        else if (computerChoice == "paper" && humanChoice == "rock") {
+            console.log("paper beats rock, you lose!");
+            ++computerScore ;
+        }
+        else if (computerChoice == "scissors" && humanChoice == "rock") {
+            console.log("rock beats scissors, you win!");
+            ++humanScore ;
+        }
+        else if (computerChoice == "scissors" && humanChoice == "paper") {
+            console.log("scissors beats paper, you lose!");
+            ++computerScore ;
+        }
+        else if (computerChoice == humanChoice) {
+            console.log("tie!")
+        }
+        else {
+            console.log("try again!")
+        }
+
+        console.log("computer score: " + computerScore);
+        console.log("your score: " + humanScore);
+        console.log(" ");
+    }
+
+    //Round One
+
+    playRound(computerChoice, humanChoice);
+
+    //Round Two
+
+    playRound(computerChoice, humanChoice);
+
+    //Round Three
+
+    playRound(computerChoice, humanChoice);
+
+    //Round Four
+
+    playRound(computerChoice, humanChoice);
+
+    //Round Five
+
+    playRound(computerChoice, humanChoice);
+
 }
 
 // const humanSelection = getHumanChoice();
 // const computerSelection = getComputerChoice();
 
-playRound(computerChoice, humanChoice);
+// console.log(randomNumber);
 
-console.log(randomNumber);
-console.log("computer chooses: " + computerChoice);
-console.log("you choose: " + humanChoice);
-
-console.log("computer score: " + computerScore);
-console.log("human score: " + humanScore);
+playGame();
