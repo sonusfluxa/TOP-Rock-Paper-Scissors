c007befc7b0db3dc648db62191669d9db24ea523
sonusfluxa <31449573+sonusfluxa@users.noreply.github.com>
2025-02-27 13:14:28 -0500

    add logic for game round in js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -19,7 +19,7 @@ let computerChoice = getComputerChoice(randomNumber);
 // prompt("rock paper scissors?")
 
 function getHumanChoice() {
-    let choice = prompt("rock paper scissors?")
+    let choice = prompt("rock paper scissors?").toLowerCase()
     
     if (choice == "rock") {
         return "rock"
@@ -38,8 +38,47 @@ let humanChoice = getHumanChoice();
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(computerChoice, humanChoice) {
+if (computerChoice == "rock" && humanChoice == "paper") {
+    console.log("paper beats rock, you win!");
+    ++humanScore ;
+}
+else if (computerChoice == "rock" && humanChoice == "scissors") {
+    console.log("rock beats scissors, you lose!");
+    ++computerScore ;
+}
+else if (computerChoice == "paper" && humanChoice == "scissors") {
+    console.log("scissors beats paper, you win!");
+    ++humanScore ;
+}
+else if (computerChoice == "paper" && humanChoice == "rock") {
+    console.log("paper beats rock, you lose!");
+    ++computerScore ;
+}
+else if (computerChoice == "scissors" && humanChoice == "rock") {
+    console.log("rock beats scissors, you win!");
+    ++humanScore ;
+}
+else if (computerChoice == "scissors" && humanChoice == "paper") {
+    console.log("scissors beats paper, you lose!");
+    ++computerScore ;
+}
+else if (computerChoice == humanChoice) {
+    console.log("tie!")
+}
+else {
+    console.log("try again!")
+}
+}
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+playRound(computerChoice, humanChoice);
+
 console.log(randomNumber);
 console.log("computer chooses: " + computerChoice);
 console.log("you choose: " + humanChoice);
-console.log(humanScore);
-console.log(computerScore);
+
+console.log("computer score: " + computerScore);
+console.log("human score: " + humanScore);
